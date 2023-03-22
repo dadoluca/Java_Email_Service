6aa8f77c736deee0eff4f1994df90265af8fb632
riadMuska <118440198+riadMuska@users.noreply.github.com>
2023-03-22 13:56:40 +0100

    interfaccia grafica

--- MailClient/src/main/java/module-info.java
+++ MailClient/src/main/java/module-info.java
@@ -2,7 +2,10 @@ module prog3.project.mailclient {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens prog3.project.mailclient to javafx.fxml;
     exports prog3.project.mailclient;
+    exports prog3.controller;
+    opens prog3.controller to javafx.fxml;
+    exports prog3.model;
+    opens prog3.model to javafx.fxml;
 }
---
+++ MailClient/src/main/java/prog3/model/Utente.java
@@ -0,0 +1,6 @@
+package prog3.model;
+
+public class Utente {
+    String mail;
+    String password;
+}
