714e92acac358f1296add1ccd5bdc021070da979
Luca Dadone <luca.dadone01@gmail.com>
2023-05-12 15:54:18 +0200

    levato parte vecchia commentata

--- src/main/java/com/example/mailservice/lib/Mailbox.java
+++ src/main/java/com/example/mailservice/lib/Mailbox.java
@@ -1,17 +1,13 @@
 package com.example.mailservice.lib;
 
-import javafx.beans.Observable;
-import javafx.beans.property.ListProperty;
-import javafx.beans.property.SimpleListProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
+
 
 public class Mailbox {
     private final StringProperty emailAddress = new SimpleStringProperty();
@@ -22,9 +18,6 @@ public class Mailbox {
     public final void setEmailAddress(final String emailAddress){
         this.emailAddressProperty().set(emailAddress);
     }
-    /*private final ObservableList<Email> emailsList = FXCollections.observableArrayList(email ->
-            new Observable[] {email.getId(), email.getSender(), });*/
-    //private final ObservableList<Email> emailsList;
 
     private final ObservableList<Email> emailList;
     public ArrayList<Email> getEmailList() {
@@ -34,22 +27,10 @@ public class Mailbox {
         return emailList;
     }
 
-//    private final ListProperty<Email> emailListProperty;
-//    public ObservableList<Email> getEmailsList() {
-//        return emailsList ;
-//    }
-//    public ListProperty<Email> emailListProperty() {
-//        return emailListProperty;
-//    }
-    /*public List<Email> getEmailsList() {
-        return emailList;
-    }*/
-
     public Mailbox(String emailAddress) {
         setEmailAddress(emailAddress);
         this.emailList = FXCollections.observableList(new LinkedList<>());
-//        this.emailListProperty = new SimpleListProperty<>();
-//        this.emailListProperty.set(emailsList);
+
     }
     public void addEmail(Email email) {
         this.emailList.add(email);
