b2d5af74cf1627b1e2751cb504fb5af3c9d43180
Davide Benotto <davide.benotto404@edu.unito.it>
2023-05-23 15:40:23 +0200

    immagine notifica

--- src/main/java/com/example/mailservice/mailclient/Client.java
+++ src/main/java/com/example/mailservice/mailclient/Client.java
@@ -225,7 +225,7 @@ public class Client {
 
     public static void showAlert(Email e, String client){
 
-        String imagePath = System.getProperty("user.dir") + "/src/main/java/com/example/mailservice/mailclient/assets/user.jpg";
+        String imagePath = System.getProperty("user.dir") + "/src/main/java/com/example/mailservice/mailclient/assets/mail.png";
 
         ImageView imageView = new ImageView(imagePath);
         imageView.setFitWidth(50); // Imposta la larghezza desiderata dell'immagine
